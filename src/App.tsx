dcfeb568410062608c6048a964211b6d5685db81
Halit <halitderyaa@gmail.com>
2023-08-15 14:17:06 +0100

    big bang

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn sdfdsf
+      </p>
+    </>
+  );
+}
+
+export default App;
