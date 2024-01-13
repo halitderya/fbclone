492db1b20592b084d43f2507977bdd6cef96b153
Halit Derya <halitderyaa@gmail.com>
2024-01-13 14:58:36 +0000

    last

--- src/components/containers/RightColumn/Contact.tsx
+++ src/components/containers/RightColumn/Contact.tsx
@@ -1,7 +1,5 @@
 import styled from "styled-components";
-import { ButtonCircle } from "../../particles/Buttons";
 const ContactItem = styled.div``;
-const ContactLabel = styled.label``;
 
 export default function ContactItemFC() {
   return ContactItem;
